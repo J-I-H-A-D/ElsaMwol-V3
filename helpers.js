8bafcf5ae7a85cf640fd7cfa6255e7894127ac19
JiHaD SeR <85656190+J-I-H-A-D@users.noreply.github.com>
2022-01-06 04:26:33 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Julie Mwol*:  ```" + msg + "```"
+    return "✅ *Elsa Mwol*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Julie Mwol*:  ```" + msg + "```"
+    return "🛑 *Elsa Mwol*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Julie Mwol*:  ```" + msg + "```"
+    return "⏺️ *Elsa Mwol*:  ```" + msg + "```"
 }
 
 
